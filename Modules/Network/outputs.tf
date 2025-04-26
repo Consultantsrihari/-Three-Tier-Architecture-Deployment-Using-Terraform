5b954d93bbe829f1b55dfa08275d65c6048b9b5b
Srihari <160323139+Consultantsrihari@users.noreply.github.com>
2025-04-26 12:38:18 +0530

    Create outputs.tf

---
+++ Modules/Network/outputs.tf
@@ -0,0 +1,20 @@
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  description = "List of IDs of the public subnets"
+  value       = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  description = "List of IDs of the private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+# Add other outputs if needed, e.g., AZ names used
+output "availability_zones" {
+  description = "List of Availability Zones used"
+  value       = var.availability_zones
+}
