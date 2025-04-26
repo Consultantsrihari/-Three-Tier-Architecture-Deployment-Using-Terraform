16386bff3140992030592c20d82658e58f216e65
Srihari <160323139+Consultantsrihari@users.noreply.github.com>
2025-04-26 12:37:48 +0530

    Create variables.tf

---
+++ Modules/Network/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "Project name prefix for resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "List of Availability Zones to use"
+  type        = list(string)
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
